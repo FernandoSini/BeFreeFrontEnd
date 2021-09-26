afc986f5b2ec290419f220b4ef04a8efb08f6fea
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2021-09-26 19:27:51 -0300

    changing enviroment

--- lib/Api/Api.dart
+++ lib/Api/Api.dart
@@ -1,5 +1,5 @@
 class Api {
-  // final url = "https://befree-backend.herokuapp.com/";
-  final String url = "http://192.168.0.22:3000/";
+  final url = "https://befree-backend.herokuapp.com/";
+  // final String url = "http://192.168.0.22:3000/";
   final String key = "url";
 }
